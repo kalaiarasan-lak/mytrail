c29601732b7b1508d882bd6a7e707b2500b8a4e1
kalaiarasan-lak <kalaibino641995@gmail.com>
2026-05-27 11:48:40 +0530

    fix github acn

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Node.js App Running Successfully</h1>');
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
